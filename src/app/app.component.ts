cd57289e35b084585aca09aec4057c614be7b9e0
Κωνσταντίνος Ιούλιος Πόρος <95686313+konstantinosporo@users.noreply.github.com>
2024-10-22 15:08:21 +0300

    added toast in bootraps js-css / implemented global error handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,20 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from "./components/footer/footer.component";
 import { HeaderComponent } from "./components/header/header.component";
+import { GlobalErrorComponent } from "./components/global-error/global-error.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, GlobalErrorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   public title = 'app-library';
+
+  handleClick() {
+    throw new Error('Here is the error message displayed!');
+  }
+
 }
---
+++ src/app/components/global-error/global-error.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-global-error',
+  standalone: true,
+  imports: [],
+  templateUrl: './global-error.component.html',
+  styleUrl: './global-error.component.css'
+})
+export class GlobalErrorComponent {
+
+}
