b1792437de15158c3ac740690dc072bb2fa41539
konstantinosporo <konstantinosporo@hotmail.gr>
2024-11-03 13:11:42 +0200

    added search/filters/return by warnings/overdue warning

--- src/app/reservations/reservations.component.ts
+++ src/app/reservations/reservations.component.ts
@@ -1,24 +1,24 @@
 import { AsyncPipe, DatePipe, NgClass, UpperCasePipe } from '@angular/common';
 import { Component, OnDestroy } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { filter, map, Observable, Subject, takeUntil } from 'rxjs';
 import { CrudActions } from '../_lib/interfaces';
 import { AlertService } from '../services/alert-handlers/alert.service';
 import { ReservationHttpService } from '../services/reservations/reservation-http.service';
-import { AddNewButtonComponent } from "../shared/buttons/add-new-button/add-new-button.component";
-import { RefreshPageButtonComponent } from "../shared/buttons/refresh-page-button/refresh-page-button.component";
-import { SpinnerComponent } from "../shared/spinner/spinner.component";
-import { ReservationApi, Status } from './reservation';
-import { ThemeService } from '../services/theme/theme.service';
-import { SearchComponent } from "../shared/search/search.component";
 import { SearchStateService } from '../services/search-state.service';
+import { ThemeService } from '../services/theme/theme.service';
 import { ActionsDropdownComponent } from "../shared/buttons/actions-dropdown/actions-dropdown.component";
 import { DropdownActions } from '../shared/buttons/actions-dropdown/dropdown';
+import { AddNewButtonComponent } from "../shared/buttons/add-new-button/add-new-button.component";
+import { RefreshPageButtonComponent } from "../shared/buttons/refresh-page-button/refresh-page-button.component";
+import { SearchComponent } from "../shared/search/search.component";
+import { SpinnerComponent } from "../shared/spinner/spinner.component";
+import { ReservationApi, Status } from './reservation';
 
 @Component({
   selector: 'app-reservations',
   standalone: true,
-  imports: [AsyncPipe, DatePipe, NgClass, UpperCasePipe, SpinnerComponent, RefreshPageButtonComponent, AddNewButtonComponent, SearchComponent, ActionsDropdownComponent],
+  imports: [AsyncPipe, DatePipe, NgClass, UpperCasePipe, SpinnerComponent, RefreshPageButtonComponent, AddNewButtonComponent, SearchComponent, ActionsDropdownComponent, RouterLink],
   templateUrl: './reservations.component.html',
   styleUrl: './reservations.component.css'
 })
@@ -26,10 +26,10 @@ export class ReservationsComponent implements CrudActions, OnDestroy {
   private readonly destroy$ = new Subject<void>();
   private readonly allReservations$!: Observable<ReservationApi[]>;
   dropdownActions: DropdownActions[] = [
-    { title: 'Active', icon: 'bi-circle' },
-    { title: 'Completed', icon: 'bi bi-check-circle' },
-    { title: 'Ascending', icon: 'bi bi-arrow-up' },
-    { title: 'Descending', icon: 'bi bi-arrow-down' },
+    { id: 'active', title: 'Active', icon: 'bi-circle' },
+    { id: 'completed', title: 'Completed', icon: 'bi bi-check-circle' },
+    { id: 'asc', title: 'Ascending', icon: 'bi bi-arrow-up' },
+    { id: 'desc', title: 'Descending', icon: 'bi bi-arrow-down' },
   ]
   status!: Status;
   reservations$!: Observable<ReservationApi[]>;
@@ -68,9 +68,26 @@ export class ReservationsComponent implements CrudActions, OnDestroy {
   delete(id: string) {
     console.log(id);
   }
-
   refreshCustomers() {
     this.reservations$ = this.reservationHttpService.getReservations();
+  }
+  getClickedDropdownId(id: string) {
+    //console.log(id + '' + 'from father comp');
+    switch (id) {
+      case ('active'):
+        this.fetchFilteredActiveReservations();
+        break;
+      case ('completed'):
+        this.fetchFilteredCompletedReservations();
+        break;
+      case ('asc'):
+        this.fetchFilteredAscReservations();
+        break;
+      case ('desc'):
+        this.fetchFilteredDescReservations();
+        break;
+      default: break;
+    }
   }
   /**
    * @konstantinosporo
@@ -117,12 +134,14 @@ export class ReservationsComponent implements CrudActions, OnDestroy {
           reservations.filter((reservation) => {
             const bookName = reservation?.book?.name?.toLowerCase() || '';
             const authorName = reservation?.book?.author?.toLowerCase() || '';
+            const customerName = reservation?.customer?.name.toLowerCase() || '';
             const status = reservation?.status?.toLowerCase() || '';
             const searchText = text.toLowerCase();
 
             return (
               bookName.includes(searchText) ||
               authorName.includes(searchText) ||
+              customerName.includes(searchText) ||
               status === searchText
             );
           })
@@ -130,7 +149,88 @@ export class ReservationsComponent implements CrudActions, OnDestroy {
       );
     }
   }
+  /**
+   * @konstantinosporo
+   * @description
+   * Filter active reservations method.
+   */
+  fetchFilteredActiveReservations() {
+    //console.log('clicked active filter!');
+    this.reservations$ = this.allReservations$.pipe(
+      map((reservations) =>
+        reservations.filter((reservation) => {
+          return reservation.status === Status.ACTIVE;
+        })
+      )
+    );
+  }
+  /**
+   * @konstantinosporo
+   * @description
+   * Filter completed reservations method.
+   */
+  fetchFilteredCompletedReservations() {
+    //console.log('clicked completed filter!');
+    this.reservations$ = this.allReservations$.pipe(
+      map((reservations) =>
+        reservations.filter((reservation) => {
+          return reservation.status === Status.COMPLETED;
+        })
+      )
+    );
+  }
+  /**
+   * @konstantinosporo
+   * @description
+   * Filter reservations in ascending order by Reserved On date.
+   */
+  fetchFilteredAscReservations() {
+    this.reservations$ = this.allReservations$.pipe(
+      map((reservations) =>
+        reservations.slice().sort((a, b) => {
+          const dateA = new Date(a.reservedOn ?? 0);
+          const dateB = new Date(b.reservedOn ?? 0);
+          return dateA.getTime() - dateB.getTime(); // Ascending order
+        })
+      )
+    );
+  }
+  /**
+ * @konstantinosporo
+ * @description
+ * Filter reservations in descending order by Reserved On date.
+ */
+  fetchFilteredDescReservations() {
+    this.reservations$ = this.allReservations$.pipe(
+      map((reservations) =>
+        reservations.slice().sort((a, b) => {
+          const dateA = new Date(a.reservedOn ?? 0);
+          const dateB = new Date(b.reservedOn ?? 0);
+          return dateB.getTime() - dateA.getTime(); // Descending order
+        })
+      )
+    );
+  }
+  /**
+   * @konstantinosporo
+   * @description Checks either a book has passed return date.
+   */
+  checkOverdue(reservation: ReservationApi): boolean {
+    return (new Date(reservation.returnBy) < new Date());
+  }
+  /*
+   * @konstantinosporo
+   * @description Change color of return by date to notify that a book is close to return.
+   */
+  getReturnByInfo(reservation: ReservationApi): { notify: string, class?: string, progress: number } {
+    const daysLeft = (new Date(reservation.returnBy).getTime() - new Date().getTime()) / (1000 * 3600 * 24);
 
+    if (daysLeft < 0) return { notify: 'Return date is overdue.', class: 'progress-red', progress: 100 };
+    if (daysLeft <= 7) return { notify: 'Return date is less than a week away.', class: 'progress-orange', progress: 70 };
+    if (daysLeft <= 30) return { notify: 'Return date is less than a month away.', class: 'progress-yellow', progress: 40 };
+
+    return { notify: 'Return date is more than a month away.', class: 'progress-green', progress: 10 };
+  }
   /**
    * @konstantinosporo
    * @description Catching the ng destroy hook to terminate subsriptions on destroy.
