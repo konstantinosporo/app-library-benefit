4e5fce69348dc189d296f4ed90f67ecdc7cd5b3d
KyriakosKl <klapsiskyriakos@gmail.com>
2024-11-07 16:43:35 +0200

    QrCode fixation button

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertService } from '../../services/alert-handlers/alert.service';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-footer',
@@ -8,12 +10,14 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+
+  constructor(private readonly alertService: AlertService ) { }
   socialsList: { title: string, icon: string, href: string }[] = [
-    {
-      title: "Facebook",
-      icon: "bi bi-facebook",
-      href: "https://www.facebook.com"
-    },
+    // {
+    //   title: "Facebook",
+    //   icon: "bi bi-facebook",
+    //   href: "https://www.facebook.com"
+    // },
     {
       title: "Twitter",
       icon: "bi bi-twitter-x",
@@ -24,11 +28,11 @@ export class FooterComponent {
       icon: "bi bi-google",
       href: "https://www.google.com"
     },
-    {
-      title: "Instagram",
-      icon: "bi bi-instagram",
-      href: "https://www.instagram.com"
-    },
+    // {
+    //   title: "Instagram",
+    //   icon: "bi bi-instagram",
+    //   href: "https://www.instagram.com"
+    // },
     {
       title: "LinkedIn",
       icon: "bi bi-linkedin",
@@ -38,10 +42,22 @@ export class FooterComponent {
       title: "GitHub",
       icon: "bi bi-github",
       href: "https://www.github.com"
+    },
+    {
+      title: "QrCode",
+      icon: "bi bi-qr-code",
+      href: "#"
     }
   ];
 
   get currentYear() {
     return new Date().getFullYear();
   }
+
+  handleQR() {
+    console.log('asdfsdfsdfsf');
+    this.alertService.showQrModal('Qr Code')
+  }
+
+
 }
